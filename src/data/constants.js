bc23170c0c377d55d279d6becc9ed3dfa5dbe9a3
SachinKumarMishra87 <sachinmishra8707@gmail.com>
2025-02-23 00:54:32 +0530

    chen

--- src/data/constants.js
+++ src/data/constants.js
@@ -193,7 +193,7 @@ export const Bio = {
       tags: ["React Js", "API"],
       category: "web app",
       github: "https://github.com/SachinKumarMishra87/countries-api",
-      // webapp: "https://podstream.netlify.app/",
+      webapp: "https://countriesapi-proje.netlify.app/",
     },
     {
       id: 5,
